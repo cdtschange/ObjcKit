baa1b00235a00704e6675dd66ab40b70c09882ef
cdts_change <cdts_change@live.cn>
2015-02-08 17:47:25 +0800

    add badge view

---
+++ UI/View/MGBadgeView/UIView+MGBadgeView.h
@@ -0,0 +1,50 @@
+//
+//  UIView+MGBadgeView.h
+//  purl
+//
+//  Created by Matteo Gobbi on 28/05/2014.
+//  Copyright (c) 2014 Matteo Gobbi (Voxygen). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_ENUM(NSUInteger, MGBadgePosition) {
+    MGBadgePositionTopLeft,
+    MGBadgePositionTopRight,
+    MGBadgePositionCenterLeft,
+    MGBadgePositionCenterRight,
+    MGBadgePositionBottomLeft,
+    MGBadgePositionBottomRight,
+    MGBadgePositionBest
+};
+
+
+@interface MGBadgeView : UIView
+
+@property (nonatomic) MGBadgePosition position;
+
+@property (nonatomic, copy) NSString *badgeValue;
+
+@property(strong, nonatomic) UIFont *font;
+
+@property(strong, nonatomic) UIColor *badgeColor;
+
+@property(strong, nonatomic) UIColor *textColor;
+
+@property(strong, nonatomic) UIColor *outlineColor;
+
+@property (nonatomic) float outlineWidth;
+
+@property (nonatomic) float minDiameter;
+
+@property (nonatomic) BOOL displayIfZero;
+
+@end
+
+
+@interface UIView (MGBadgeView)
+
+@property(nonatomic, readonly) MGBadgeView *badgeView;
+
+@end
